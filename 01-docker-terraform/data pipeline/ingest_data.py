9e0c85cfd7bea69e38854dc659c69ad7a0bb7af6
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2026-01-17 02:20:18 +0000

    Added click integration

--- 01-docker-terraform/data pipeline/ingest_data.py
+++ 01-docker-terraform/data pipeline/ingest_data.py
@@ -4,6 +4,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+import click
 
 dtype = {
     "VendorID": "Int64",
@@ -29,19 +30,17 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-def run():
-    pg_user = 'root'
-    pg_password = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
-
-    year = 2021
-    month = 1
-    target_table = 'yellow_taxi_data'
-
-    chunksize = 100000
-
+@click.command()
+@click.option('--pg_user', default='root', help='PostgreSQL username')
+@click.option('--pg_password', default='root', help='PostgreSQL password')
+@click.option('--pg_host', default='localhost', help='PostgreSQL host')
+@click.option('--pg_port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg_db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--target_table', default='yellow_taxi_data', help='Target table name')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for data ingestion')
+def run(pg_user, pg_password, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
 
@@ -52,7 +51,7 @@ def run():
         dtype=dtype,
         parse_dates=parse_dates,
         iterator=True,
-        chunksize=100000
+        chunksize=chunksize
     )
 
     first = True
