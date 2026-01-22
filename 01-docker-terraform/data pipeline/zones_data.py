16576ca4b280ac773644b48e4c378766d333b710
Chukwuemeka Okoli <32660991+chuksoo@users.noreply.github.com>
2026-01-22 08:22:38 +0000

    Added ingestion script to download zones data

---
+++ 01-docker-terraform/data pipeline/zones_data.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
+import click
+
+
+@click.command()
+@click.option('--pg_user', default='root', help='PostgreSQL username')
+@click.option('--pg_password', default='root', help='PostgreSQL password')
+@click.option('--pg_host', default='localhost', help='PostgreSQL host')
+@click.option('--pg_port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg_db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--target_table', default='yellow_taxi_data', help='Target table name')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for data ingestion')
+def run(pg_user, pg_password, pg_host, pg_port, pg_db, target_table, chunksize):
+    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/'
+    url = f'{prefix}/taxi_zone_lookup.csv'
+
+    engine = create_engine(f'postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}')
+    
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize
+    )
+
+    first = True
+    for df_chunk in tqdm(df_iter):
+        if first:
+            df_chunk.head(n=0).to_sql(
+                name=target_table, 
+                con=engine, 
+                if_exists='replace'
+            )
+            first = False
+
+        df_chunk.to_sql(
+            name=target_table, 
+            con=engine, 
+            if_exists='append'
+        )
+
+
+if __name__ == '__main__':
+    run()
+
+
+
