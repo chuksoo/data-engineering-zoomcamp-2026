09cc6abde5de646b478f2face73960eb17e1e698
chuksoo <mexx4me@yahoo.com>
2026-01-25 13:38:16 -0900

    Added terraform main script

---
+++ 01-docker-terraform/terraform/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "7.16.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "terraform-dezoomcamp-485401"
+  region  = "us-central1"
+}
