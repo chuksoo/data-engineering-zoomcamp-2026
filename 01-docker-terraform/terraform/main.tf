c287b4a00fbc21e9d8e1264b075220ea493b952e
chuksoo <mexx4me@yahoo.com>
2026-01-25 16:35:51 -0900

    Added terraform resources

--- 01-docker-terraform/terraform/main.tf
+++ 01-docker-terraform/terraform/main.tf
@@ -8,6 +8,22 @@ terraform {
 }
 
 provider "google" {
+  # credentials = file("./keys/my-creds.json") 
   project = "terraform-dezoomcamp-485401"
   region  = "us-central1"
+}
+
+resource "google_storage_bucket" "dezoomcamp-bucket" {
+  name          = "terraform-dezoomcamp-485401-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
 }
